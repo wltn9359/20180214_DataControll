06d4d829c6166ecb3901daa2e4fb8ee17672bbef
Arkuma <jisu08451@gmail.com>
2018-02-14 17:43:37 +0900

    추가추

---
+++ 20180214_DataControll/Assets/Scripts/TestCSVRead.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Text;
+
+
+public class TestCSVRead : MonoBehaviour {
+
+    public string csvFileName;
+    public int Ran;
+    public GameObject unitPrefabs;
+ 
+
+	void Start ()
+    {
+   
+        List<Dictionary<string, object>> data = CSVReader.Read(csvFileName);
+        //Debug.Log(0 + "번의" + "종족은:::" + data[0]["종족"] + "이름은:::" + data[0]["유닛이름"] + ":::" + "HP:::" + data[0]["체력"]);
+
+        //for (int i = 0; i < data.Count; i++)
+        //{
+        //    Debug.Log(i + "번의" + "종족은:::" + data[i]["종족"] + "이름은:::" + data[i]["유닛이름"]+":::"+":::"+"HP:::"+data[i]["체력"]);
+        //}
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            GameObject unitOBJ = Instantiate(unitPrefabs) as GameObject;
+            UnitScripts unitDetail = unitOBJ.GetComponent<UnitScripts>();
+            switch (data[i]["종족"].ToString())
+            {
+
+                case "프로토스":
+                    unitDetail.unitTribe = UnitScripts.UNITTRIBE.PROTOSS;
+                    break;
+                case "저그":
+                    unitDetail.unitTribe = UnitScripts.UNITTRIBE.ZERG;
+                    break;
+                case "테란":
+                   unitDetail.unitTribe = UnitScripts.UNITTRIBE.TERRAN;
+                    break;
+            }
+
+            unitDetail.unitName = data[i]["유닛이름"].ToString();
+            unitDetail.unitHP = int.Parse(data[i]["체력"].ToString());
+            unitDetail.unitMP = int.Parse(data[i]["마나"].ToString());
+            unitDetail.unitATK = int.Parse(data[i]["공격력"].ToString());
+            unitDetail.unitDEF = int.Parse(data[i]["방어력"].ToString());
+            unitDetail.unitSHD = int.Parse(data[i]["쉴드"].ToString());
+            unitDetail.unitASP = float.Parse(data[i]["공격속도"].ToString());
+            unitDetail.unitMSP = float.Parse(data[i]["이동속도"].ToString());
+            unitDetail.unitRAN = float.Parse(data[i]["사거리"].ToString());
+        }
+    }
+}
---
+++ 20180214_DataControll/Assets/Scripts/UnitScripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScripts : MonoBehaviour {
+
+    public enum UNITTRIBE
+    {
+        NEUTRAL=0,
+        PROTOSS,
+        ZERG,
+        TERRAN
+    }
+
+    public UNITTRIBE unitTribe; // 종족
+    public string unitName;//유닛이름
+    public int unitHP;//체력
+    public int unitMP;//마나
+    public int unitATK;//공격력
+    public int unitDEF;//방어력
+    public int unitSHD;//쉴드
+    public float unitASP;//공격속도
+    public float unitMSP;//이동속도
+    public float unitRAN;//사거리
+
+	void Start ()
+    {
+        gameObject.name = unitName;
+	}
+	
+	}
